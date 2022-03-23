818acf2ef93856c54c2a84dffa9c718a381b953f
xuhongxi <2696861203@qq.com>
2022-03-23 16:38:01 +0800

    one commit

---
+++ src/main/java/com/atguigu/test/TestMain.java
@@ -0,0 +1,14 @@
+package com.atguigu.test;
+
+/**
+ * @author 尚硅谷学员-it梦想
+ * @Title:
+ * @date 2022/3/23 15:59
+ * @Version 1.0
+ */
+public class TestMain {
+
+    public static void main(String[] args) {
+        System.out.println("你好 git");
+    }
+}
